71f5cdbd13d94edf28775f67c0fd897b74bddfa0
Julián Amé <julianmmame@gmail.com>
2023-01-31 20:44:35 -0300

    transfiero data sensible a un env no commiteado

--- src/configs/environment.mjs
+++
@@ -1,7 +0,0 @@
-export const MAIL_DATA = {
-    MAILHOST: 'smtp.dreamhost.com',
-    PORT: '465',
-    USERNAME: 'contacto_website_saliente@julianmmame.com.ar',
-    PASSWORD: 'V33black5528',
-    TO: 'contacto_webpage@julianmmame.com.ar'
-}

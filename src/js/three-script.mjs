9925732637a413ca06bcd6521d0b35bb9e07d953
Julián Amé <julianmmame@gmail.com>
2023-02-02 01:54:29 -0300

    trabajando en el threejs

--- src/js/three-script.mjs
+++ src/js/three-script.mjs
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import {
     OrbitControls
 } from 'three/examples/jsm/controls/OrbitControls';
+import {
+    DRACOLoader
+} from 'three/examples/jsm/loaders/dracoloader';
 
 /*
 import * as dat from 'dat.gui';
@@ -48,6 +51,7 @@ window.addEventListener('resize', function () {
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.enableZoom = false;
 camera.position.set(0, 0, 10);
+orbit.target.set(0, 1, 3);
 orbit.update();
 
 
@@ -66,16 +70,16 @@ scene.add(light1Helper);
 */
 
 //Agrego una ambient light
-const light2 = new THREE.AmbientLight(0x222222, 0.8);
+const light2 = new THREE.AmbientLight(0x222222, .9);
 scene.add(light2);
-light2.position.set(0,0,10);
+light2.position.set(0, 0, 10);
 light2.castShadow = true;
 light2.angle = 0.2;
 
 //Agrego una luz direccional
-const directionaLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+const directionaLight = new THREE.DirectionalLight(0xFFFFFF, 1.5);
 scene.add(directionaLight);
-directionaLight.position.set(0,0,10);
+directionaLight.position.set(0, 0, 10);
 directionaLight.castShadow = true;
 directionaLight.shadow.camera.left = 0;
 /*
@@ -113,14 +117,41 @@ const personajeURL = new URL('../3dModels/character_standing.glb',
 //Cargo el importador gltf.
 const assetLoader = new GLTFLoader();
 
+/*
+//Si el Objeto 3d está comprimido, tengo uqe descomprimirlo
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('three/examples/jsm/libs/draco/');
+assetLoader.setDRACOLoader(dracoLoader);
+*/
+
+
 //Cargo el objeto 3d
-assetLoader.load(personajeURL.href, function (gltf) {
-    const model = gltf.scene;
-    scene.add(model);
-    model.position.set(1.5, 0.2, 0);
-}, undefined, function (error) {
-    console.error(error);
-});
+assetLoader.load(personajeURL.href,
+    function (gltf) {
+        //Función llamada cuando el recurso se carga
+        const model = gltf.scene;
+        scene.add(model);
+        model.position.set(1.2, 0.20, 0);
+
+
+        const mixer = new THREE.AnimationMixer(model);
+        const animations = gltf.animations;
+        const animation = THREE.AnimationClip.findByName(animations, 'idle');
+
+        const action = mixer.clipAction(animation);
+        action.play();
+
+
+
+    },
+    function (xhr) {
+        // Función llamada cuando la carga progresa
+        //console.log( Math.round( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function (error) {
+        //Función llamada cuando hubo un error
+        console.error(error);
+    });
 
 
 
@@ -137,6 +168,8 @@ function animate(time) {
     */
 
     //cubeGhost1.rotation.set(time/10000, time/10000, time/10000)
+
+    requestAnimationFrame(animate);
 
     renderer.render(scene, camera);
 

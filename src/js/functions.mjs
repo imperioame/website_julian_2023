2f3cd39e05fe45e0a779bcd72fa2bf2ffa1899b4
Julián Amé <julianmmame@gmail.com>
2023-02-12 16:36:08 -0300

    intentando otra cosa para el captcha

--- src/js/functions.mjs
+++ src/js/functions.mjs
@@ -263,7 +263,7 @@ export function sendEmail(e) {
         `${document.getElementById('contact_form_email').value}`,
         `${document.getElementById('contact_form_nombre').value}`,
         "Contacto a través de la página web de " + `${document.getElementById('contact_form_nombre').value}`,
-        `${document.getElementById('contact_form_mensaje').value}`
+        `${document.getElementById('contact_form_mensaje').value}`,  grecaptcha.getResponse()
     );
 
     console.log(result);
--- src/services/mail.mjs
+++ src/services/mail.mjs
@@ -1,8 +1,30 @@
 import {
     callTooltip
 } from '../js/sections/tooltip.mjs';
+const axios = require("axios");
 
-export function sendMail(from, name, subject, text) {
+async function verifyRecaptcha(recaptcha) {
+  try {
+    const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", {
+      secret: "6Le3U24kAAAAAJvrR7cRu6imFMEkYgFV7Z5Ja8X5",
+      response: recaptcha
+    });
+
+    if (response.data.success) {
+      // The reCAPTCHA response is valid
+      return true;
+    } else {
+      // The reCAPTCHA response is invalid
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+
+export function sendMail(from, name, subject, text, recaptcha_value) {
     (function () {
         emailjs.init("bw-B3soQ2MARRdfFx");
     })();
@@ -12,22 +34,26 @@ export function sendMail(from, name, subject, text) {
         return 'error';
     }
 
-    var templateParams = {
-        subject: subject,
-        name: name,
-        from: from,
-        text: text,
-    };
-
-
-    emailjs.send('dreamhost_smtp', 'CF_Webpage_julian', templateParams)
-        .then(function (response) {
-            console.log('SUCCESS!', response.status, response.text);
-            callTooltip('div_form_contacto_container', 'Mensaje enviado correctamente', 'green');
-            return response;
-        }, function (err) {
-            console.log('FAILED...', err);
-            callTooltip('submit_button', 'El mensaje no se ha podido enviar', 'red');
-            return err;
-        });
+    if (!verifyRecaptcha(recaptcha_value)) {
+        return 'Error en el recaptcha';
+    }else{
+        var templateParams = {
+            subject: subject,
+            name: name,
+            from: from,
+            text: text,
+        };
+    
+    
+        emailjs.send('dreamhost_smtp', 'CF_Webpage_julian', templateParams)
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                callTooltip('div_form_contacto_container', 'Mensaje enviado correctamente', 'green');
+                return response;
+            }, function (err) {
+                console.log('FAILED...', err);
+                callTooltip('submit_button', 'El mensaje no se ha podido enviar', 'red');
+                return err;
+            });
+    }
 }

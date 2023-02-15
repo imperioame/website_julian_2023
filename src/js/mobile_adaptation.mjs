217024611ce7f1d2823714d3bcc4305697e9c967
Julián Amé <julianmmame@gmail.com>
2023-02-14 23:11:31 -0300

    correcciones menores de mobile

--- src/js/mobile_adaptation.mjs
+++ src/js/mobile_adaptation.mjs
@@ -1,25 +1,30 @@
 export function addMobileElements() {
-    const banner_mi_persona = document.getElementById('banner_mi_persona');
-    const overlay_gradiente = document.createElement('div');
-    overlay_gradiente.id = 'overlay_gradiente_mi_persona';
-    banner_mi_persona.appendChild(overlay_gradiente);
+    if (!document.getElementById('overlay_gradiente_mi_persona')) {
+        console.log('no hay overlay gradiente');
+        const banner_mi_persona = document.getElementById('banner_mi_persona');
+        const overlay_gradiente = document.createElement('div');
+        overlay_gradiente.id = 'overlay_gradiente_mi_persona';
+        banner_mi_persona.appendChild(overlay_gradiente);
+    }
 
-
-    const spacer_banner_mi_persona = document.getElementById('banner_mi_persona').getElementsByClassName('spacer-4')[0];
-    spacer_banner_mi_persona.classList.remove('spacer-4');
-    spacer_banner_mi_persona.classList.add('spacer-1');
-
+    const spacer_banner_mi_persona = document.querySelector('#banner_mi_persona .spacer-4');
+    if (spacer_banner_mi_persona) {
+        spacer_banner_mi_persona.classList.remove('spacer-4');
+        spacer_banner_mi_persona.classList.add('spacer-1');
+    }
 }
 
 
 
 export function removeMobileElements() {
-    const banner_mi_persona = document.getElementById('banner_mi_persona');
-    if(banner_mi_persona){
-        banner_mi_persona.remove();
+    const overlay_gradiente = document.getElementById('overlay_gradiente_mi_persona');
+    if (overlay_gradiente) {
+        overlay_gradiente.remove();
     }
 
-    const spacer_banner_mi_persona = document.getElementById('banner_mi_persona').getElementsByClassName('spacer-4')[0];
-    spacer_banner_mi_persona.classList.remove('spacer-1');
-    spacer_banner_mi_persona.classList.add('spacer-4');
+    const spacer_banner_mi_persona = document.querySelector('#banner_mi_persona .spacer-1');
+    if (spacer_banner_mi_persona) {
+        spacer_banner_mi_persona.classList.remove('spacer-1');
+        spacer_banner_mi_persona.classList.add('spacer-4');
+    }
 }
